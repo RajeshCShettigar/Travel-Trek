34db63fb381bfc8fb593fe14a95423712ad23024
rajesh <rajeshshettigar249@gmail.com>
2023-07-13 22:24:53 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: "/home",
+  base:"/Travel-Trek/home",
   plugins: [react()],
 })
